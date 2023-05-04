bd41ee78d17343868b6fdd38ad7b8e340486704d
Thercístenes <thercistenes1@outlook.com>
2023-05-04 15:28:41 -0300

    fundamentos/secao-03-introducao-a-javascrpt/dia-03/javascript/funcoes

---
+++ fundamentos/secao-03-introducao-a-javascript/dia3-javascript-funcoes/funcoes/script0.js
@@ -0,0 +1,5 @@
+function cumprimentaCliente(cliente) {
+    return 'Olá, ' + cliente + '. Essa é sua conta do TrybeBank'
+  }
+  
+  console.log(cumprimentaCliente('Thercístenes')) // Olá, Isaac. Essa é sua conta do TrybeBank
