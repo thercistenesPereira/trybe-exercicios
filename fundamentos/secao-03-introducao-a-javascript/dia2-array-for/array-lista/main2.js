cc2f199ce33e1bf80a027611c84bd80465a51882
Thercístenes <thercistenes1@outlook.com>
2023-05-04 11:11:33 -0300

    fundamentos/secção-03/dia-02-Arrays(lista)

---
+++ fundamentos/secao-03-introducao-a-javascript/dia2-array-for/array-lista/main2.js
@@ -0,0 +1,8 @@
+let lista = ['café', 'almoço', 'jantar']
+
+lista.push('ceia')
+lista.unshift('agua')
+
+console.log(lista)
+
+
---
+++ fundamentos/secao-03-introducao-a-javascript/dia2-array-for/array-lista/main7.js
@@ -0,0 +1,7 @@
+//Adicione o valor “Contato” no final do array menu.
+//De olho na dica 👀: Use o método .push()
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato')
+
+console.log(menu);
