90f13ea6e9d22c1d6c42bc19d313bc6974991b84
Thercístenes <thercistenes1@outlook.com>
2023-05-05 19:53:49 -0300

    fundamentos/seção-03/dia-04/objects-values e objects-keys

---
+++ fundamentos/secao-03-introducao-a-javascript/dia4-javascript-objetos/object-value/script1.js
@@ -0,0 +1,24 @@
+let coolestTvShow = {
+    name: 'BoJack Horseman',
+    genre: 'adult animation',
+    createdBy: 'Raphael Bob-Waksberg',
+    favoriteCharacter: 'Princess Carolyn',
+    quote: 'Princess Carolyn always lands on her feet.',
+    seasons: 6,
+  };
+  
+  // for (let property in coolestTvShow) {
+  //   console.log(coolestTvShow[property]);
+  // }
+  
+  console.log(Object.values(coolestTvShow));
+  
+  // [
+  //   'BoJack Horseman',
+  //   'adult animation',
+  //   'Raphael Bob-Waksberg',
+  //   'Princess Carolyn',
+  //   'Princess Carolyn always lands on her feet.'
+  //    6
+  // ]
+  
---
+++ fundamentos/secao-03-introducao-a-javascript/dia4-javascript-objetos/objetos-keys/script2.js
@@ -0,0 +1,16 @@
+let student1 = {
+  html: 'Muito Bom',
+  css: 'Bom',
+  javascript: 'Ótimo',
+  softskills: 'Ótimo',
+};
+
+let student2 = {
+  html: 'Bom',
+  css: 'Ótimo',
+  javascript: 'Ruim',
+  softskills: 'Ótimo',
+  git: 'Bom', // chave adicionada
+};
+console.log(Object.keys(student1));
+console.log(Object.keys(student2));
