d5103c19f3e5de74fd2339c5bf4de74b6420933e
Thercístenes <thercistenes1@outlook.com>
2023-05-06 10:26:49 -0300

    fundamentos/seção-03/dia-04/objects-assin/-objects-entries

---
+++ fundamentos/secao-03-introducao-a-javascript/dia4-javascript-objetos/objects-entries/script1.js
@@ -0,0 +1,8 @@
+let countries = {
+    franca: 'Paris',
+    brasil: 'Brasília',
+    espanha: 'Madrid',
+    portugal: 'Lisboa',
+  };
+  let pairKeyValue = Object.entries(countries);
+  console.log(pairKeyValue);
