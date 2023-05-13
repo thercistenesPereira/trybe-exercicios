f0b88ea8a39bbb94e8ba7e5cc3fed09c950cebcb
Thercístenes <thercistenes1@outlook.com>
2023-05-12 22:02:02 -0300

    primeira para do dia 1 da seção 4 de fundamentos concluída

---
+++ fundamentos/secao-04-dom-eventos-web-storage/dia-02-trabalhando-com-elementos/buscando-elementos/script.js
@@ -0,0 +1,5 @@
+// arquivo script.js
+
+const element = document.querySelector('#where-are-you')
+const parent = element.parentNode.style.color = 'blue'
+
---
+++ fundamentos/secao-04-dom-eventos-web-storage/dia-02-trabalhando-com-elementos/elementos-busca/script.js
@@ -0,0 +1,3 @@
+const element = document.getElementById('parent');
+const elements = element.firstChild;
+console.log(elements);
