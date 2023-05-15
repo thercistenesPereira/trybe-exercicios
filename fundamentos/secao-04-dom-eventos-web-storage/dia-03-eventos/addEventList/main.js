969ecc34ca0e3e1c25eaf1dfa3ac098a4aed5657
Thercístenes <thercistenes1@outlook.com>
2023-05-15 10:39:35 -0300

    fundamentos/secao-04/dia-03/fixando aprendizado

--- fundamentos/secao-04-dom-eventos-web-storage/dia-03-eventos/addEventList/main.js
+++ fundamentos/secao-04-dom-eventos-web-storage/dia-03-eventos/addEventList/main.js
@@ -30,9 +30,17 @@ input.addEventListener('input', (event) => {
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy',
 // redirecione para alguma página.
 // 4.1. Que tal redirecionar para seu portfólio? Dica: Lembre-se dos métodos `window.location.replace` e `window.open`.
-
+myWebpage.addEventListener('dblclick', ()=> {
+  window.location.replace('https://www.google.com/')
+});
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo.
+myWebpage.addEventListener('mouseover', (event) => {
+  event.target.style.color = 'blue';
+});
+myWebpage.addEventListener('mouseout', (event) => {
+  event.target.style.color = 'unset';
+})
 
 // Segue abaixo um exemplo de uso do event.target.
 
