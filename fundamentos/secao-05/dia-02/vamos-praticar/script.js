cd6456ef9fcb085b4b507e2969c936c058d056ee
Thercístenes <thercistenes1@outlook.com>
2023-05-22 16:27:22 -0300

    fundamentos/secao-05/dia-02/exercicio-complementar

---
+++ fundamentos/secao-05/dia-02/vamos-praticar/script.js
@@ -0,0 +1,53 @@
+import JustValidate from 'just-validate';
+
+const validator = new JustValidate('#basic_form');
+
+validator
+  .addField('#basic_name', [
+    {
+      rule: 'required',
+    },
+    {
+      rule: 'minLength',
+      value: 3,
+    },
+    {
+      rule: 'maxLength',
+      value: 15,
+    },
+  ])
+  .addField('#basic_email', [
+    {
+      rule: 'required',
+    },
+    {
+      rule: 'required',
+    },
+    {
+      rule: 'email',
+    },
+  ])
+  .addField('#basic_password', [
+    {
+      rule: 'required',
+    },
+    {
+      rule: 'password',
+    },
+  ])
+  .addField('#basic_age', [
+    {
+      rule: 'required',
+    },
+    {
+      rule: 'number',
+    },
+    {
+      rule: 'minNumber',
+      value: 18,
+    },
+    {
+      rule: 'maxNumber',
+      value: 150,
+    },
+  ]);
