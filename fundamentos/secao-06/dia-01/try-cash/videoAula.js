86b7f2c1ec6d27bb6dd5b88202054c761f9be85c
Thercístenes <thercistenes1@outlook.com>
2023-05-29 15:41:14 -0300

    avanço fundamentos/secao-06/dia-01/try-cash/videoAula.js

---
+++ fundamentos/secao-06/dia-01/try-cash/videoAula.js
@@ -0,0 +1,17 @@
+const verificaValores = (a, b) => {
+    if (a !== 'number' || b !== 'number') {
+        throw new Error('ERRO! Os valores precisam ser numéricos');
+    }
+};
+
+const sum = (a, b) => {
+    try {
+        verificaValores(a, b);
+        return a + b;
+    }
+    catch (erro) {
+        return erro.message;
+    }
+}
+
+console.log(sum(2, '3'));
