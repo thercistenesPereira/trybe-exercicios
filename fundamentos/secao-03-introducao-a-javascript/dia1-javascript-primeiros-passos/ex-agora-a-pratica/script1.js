0fbb416a950ccfda03065c862b19334bc6478c2d
Thercístenes <thercistenes1@outlook.com>
2023-06-02 10:37:44 -0300

    fundamentos/secao-03-introducao-a-javascript/dia1-javascript-primeiros-passos/ex-agora-a-pratica/script1.js

---
+++ fundamentos/secao-03-introducao-a-javascript/dia1-javascript-primeiros-passos/ex-agora-a-pratica/script1.js
@@ -0,0 +1,8 @@
+/*Exercício 2
+Utilize if/else para escrever um código que retorne o maior de dois números. Defina, no começo do seu código, duas variáveis com os valores que serão comparados. */ 
+
+const num1 = 5;
+const num2 = 2;
+
+const result = num1 > num2 ? `${num1} é maior que ${num2}` : `${num2} é maior que ${num1}`;
+console.log(result);
