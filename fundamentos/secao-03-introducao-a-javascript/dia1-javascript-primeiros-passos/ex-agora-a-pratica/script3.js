2ab841a0d2891345e79bd895f906272cc0d54729
Thercístenes <thercistenes1@outlook.com>
2023-06-02 11:08:59 -0300

    fundamentos/secao-03-introducao-a-javascript/dia1-javascript-primeiros-passos/ex-agora-a-pratica/script3.js

--- fundamentos/secao-03-introducao-a-javascript/dia1-javascript-primeiros-passos/ex-agora-a-pratica/script3.js
+++ fundamentos/secao-03-introducao-a-javascript/dia1-javascript-primeiros-passos/ex-agora-a-pratica/script3.js
@@ -3,3 +3,17 @@ Utilize if...else para escrever um código que defina três variáveis com os va
 
 👀 Dica: para os ângulos serem de um triângulo válido, a soma dos três ângulos deve ser 180 graus. Um ângulo será considerado inválido se não tiver um valor positivo. */
 
+// const { log } = require("console");
+
+const angle1 = -10;
+const angle2 = 45;
+const angle3 = 90;
+
+const sumAngle = angle1 + angle2 + angle3;
+const anglePositive = angle1 > 0 && angle2 > 0 && angle3 > 0;
+
+if (anglePositive) {
+    sumAngle === 180 ? console.log(true) : console.log(false);
+} else {
+    console.log("Algum ângulo é negativo");
+}
