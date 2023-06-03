fd8cfdc719b52627bea018380f03ed117c2e59c4
Thercístenes <thercistenes1@outlook.com>
2023-06-03 14:22:27 -0300

    fundamentos/secao-03-introducao-a-javascript/dia3-javascript-funcoes/gus-logica/

---
+++ fundamentos/secao-03-introducao-a-javascript/dia3-javascript-funcoes/gus-logica/1-operacoesAritimeticas.js
@@ -0,0 +1,7 @@
+let num1 = 5;
+let num2 = 2;
+
+console.log(`Soma: ${num1} + ${num2} = ${num1 + num2}`);
+console.log(`Multiplicação: ${num1} x ${num2} = ${num1 * num2}`);
+console.log(`Subtração: ${num1} - ${num2} = ${num1 - num2}`);
+console.log(`Divisão ${num1} / ${num2} = ${num1 / num2}`);
