25b97e60895dbf6425652980b70ebc78a0fef3ae
Thercístenes <thercistenes1@outlook.com>
2023-06-03 15:47:33 -0300

    fundamentos/secao-03-introducao-a-javascript/dia3-javascript-funcoes/gus-logica/2-valor-jantar.js

---
+++ fundamentos/secao-03-introducao-a-javascript/dia3-javascript-funcoes/gus-logica/2-valor-jantar.js
@@ -0,0 +1,10 @@
+let valorJantar = 997;
+let taxaGarcon = valorJantar * 0.1;
+let valorPgt = valorJantar + taxaGarcon;
+
+console.log(`Valor do jantar: R$ ${parseFloat(valorJantar).toFixed(2)}`);
+console.log(`Taxa do garçon: R$ ${parseFloat(taxaGarcon).toFixed(2)}`);
+console.log(`Valor total a pagar: R$ ${parseFloat(valorPgt).toFixed(2)}`);
+
+
+
