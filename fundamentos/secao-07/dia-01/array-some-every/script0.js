e5418a8f4e5b1649af80ce523567e8d0d811e7ef
Thercístenes <thercistenes1@outlook.com>
2023-06-04 16:16:16 -0300

    fundamentos/secao-07/dia-01/array-some-every/

---
+++ fundamentos/secao-07/dia-01/array-some-every/script0.js
@@ -0,0 +1,10 @@
+const pessoas = [
+  { nome: 'Ana', cargo: 'Analista' },
+  { nome: 'João', cargo: 'Gerência' },
+  { nome: 'Aline', cargo: 'Analista' },
+  { nome: 'Joana', cargo: 'Gerência' },
+];
+
+const verifyCargo = pessoas.some((pessoa) => pessoa.cargo === 'programador');
+
+console.log(verifyCargo);
