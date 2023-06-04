a3442fa09b1081c1cadd0c6297d4c07d46fedb2b
Thercístenes <thercistenes1@outlook.com>
2023-06-04 19:34:28 -0300

    fundamentos/secao-07/dia-01/array-some-every/script5.js

--- fundamentos/secao-07/dia-01/array-some-every/script3.js
+++ fundamentos/secao-07/dia-01/array-some-every/script3.js
@@ -2,7 +2,7 @@
 
 const listNames = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
 
-const verifyFirstLetter = (letter, names) => names.some((name) => name[0] === letter);
+const verifyFirstLetter = (letter, names)  => names.some((name) => name[0] === letter);
 
 console.log(verifyFirstLetter('J', listNames)); // true
 console.log(verifyFirstLetter('X', listNames)); // false
---
+++ fundamentos/secao-07/dia-01/array-some-every/script4.js
@@ -0,0 +1,11 @@
+// const { toLower } = require("lodash");
+
+const grades = {
+  portugues: 'Aprovado',
+  matematica: 'Reprovado',
+  ingles: 'Aprovado',
+};
+
+const verifyGrades = Object.values(grades).every((grade) => grade === 'Aprovado'); // false
+
+console.log(verifyGrades);
