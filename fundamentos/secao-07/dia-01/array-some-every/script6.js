d9be9b4e3a8841171decd97329571aff309a88c5
Thercístenes <thercistenes1@outlook.com>
2023-06-04 19:44:24 -0300

    modified:   fundamentos/secao-07/dia-01/array-some-every/script5.js

---
+++ fundamentos/secao-07/dia-01/array-some-every/script6.js
@@ -0,0 +1,6 @@
+// Você ficou responsável por criar um sistema que verifica se as pessoas da mesma turma possuem uma determinada faixa de idade. Você deve utilizar o array people e desenvolver a sua lógica a partir dele. Para isso:
+
+// Crie uma função que verifica se todas as pessoas do array people possuem a idade mínima especificada;
+
+// Retorne true se todas tiverem a idade maior ou igual à mínima e, caso contrário, false.
+
