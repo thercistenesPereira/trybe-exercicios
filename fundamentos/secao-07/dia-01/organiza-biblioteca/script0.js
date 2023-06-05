d3c9e015f7f3ea3a9931bddd0e46bf8f7ac86dae
Thercístenes <thercistenes1@outlook.com>
2023-06-05 20:17:38 -0300

    everyoneWasBornOnSecXX

--- fundamentos/secao-07/dia-01/organiza-biblioteca/script0.js
+++ fundamentos/secao-07/dia-01/organiza-biblioteca/script0.js
@@ -1,3 +1,5 @@
+const { log } = require("console");
+
 const books = [
   {
     id: 1,
@@ -89,3 +91,12 @@ const getNamedBook = () => {
   return books.find((book) => book.name.length === 26);
 }
 console.log(getNamedBook());
+
+
+//Faça uma função que retorne true se todas as pessoas autoras tiverem nascido no século XX, ou false, caso contrário.
+// const expectedResult = false;
+function everyoneWasBornOnSecXX() {
+  return books.every((book) => (book.author.birthYear >= 1901 && book .author.birthYear <= 2000));
+}
+console.log(everyoneWasBornOnSecXX());
+
