3bc666eb4b25decd3da7692937fbbdd0227dfe72
Thercístenes <thercistenes1@outlook.com>
2023-06-09 17:20:32 -0300

    fundamentos/secao-07/dia-03/array-filter/script1.js

--- fundamentos/secao-07/dia-03/array-filter/script.js
+++ fundamentos/secao-07/dia-03/array-filter/script.js
@@ -1,3 +1,3 @@
 const dados = ['Luca', 91234567, 'Ana', 92345678, 'Marlete', 93456789];
 
-console.log(dados.filter((item) => typeof item === 'string'));
+console.log(dados.filter((item) => typeof item !== 'string'));
---
+++ fundamentos/secao-07/dia-03/array-filter/script1.js
@@ -0,0 +1,3 @@
+const listaNumeros = [10, 20, 30, 40];
+
+console.log(listaNumeros.filter((number) => number > 20));
