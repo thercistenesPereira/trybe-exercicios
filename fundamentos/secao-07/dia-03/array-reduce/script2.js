558bc97d91cc31dba3b72443f67f94c24fe546bb
Thercístenes <thercistenes1@outlook.com>
2023-06-09 20:29:16 -0300

    fundamentos/secao-07/dia-03/array-reduce/script2.js

---
+++ fundamentos/secao-07/dia-03/array-reduce/script2.js
@@ -0,0 +1,8 @@
+const numbers = [50, 85, -30, 3, 15];
+
+const getBigger = (bigger, number) => bigger > number ? bigger : number;
+
+const bigger = numbers.reduce(getBigger);
+
+console.log(bigger);
+
