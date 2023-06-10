0bf087a3aea8500ba4d9e01187573b33c6c943b5
Thercístenes <thercistenes1@outlook.com>
2023-06-10 09:41:17 -0300

    fundamentos/secao-03-introducao-a-javascript/dia4-javascript-objetos/key-values-entries/

---
+++ fundamentos/secao-03-introducao-a-javascript/dia4-javascript-objetos/key-values-entries/script.js
@@ -0,0 +1,13 @@
+const product = {
+  name: 'Ruffles Original - 76g',
+  price: 8.59,
+  company: 'Elma Chips',
+  nutritionInfo: {
+    carbohydrates: 12,
+    sugar: 0,
+    sodium: 129,
+  }
+};
+
+// console.log(Object.keys(product)); // [ 'name', 'price', 'company', 'nutritionInfo' ]
+console.log(Object.keys(product));
