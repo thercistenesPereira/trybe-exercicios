c27c41617a1110aa24b881f3f230ec2ec6aea727
Thercístenes <thercistenes1@outlook.com>
2023-06-10 09:17:39 -0300

    fundamentos/secao-03-introducao-a-javascript/dia4-javascript-objetos/objetos/script6.js

---
+++ fundamentos/secao-03-introducao-a-javascript/dia4-javascript-objetos/objetos/script6.js
@@ -0,0 +1,10 @@
+const customer = {
+  firstName: 'Roberto',
+  age: 22,
+  job: 'Software Engineer',
+};
+
+// customer.lastName = 'Faria';
+customer['lastName'] = 'Faria';
+
+console.log(customer);
