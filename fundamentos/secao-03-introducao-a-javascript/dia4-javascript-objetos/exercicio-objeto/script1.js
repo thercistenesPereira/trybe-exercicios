7bcc640028cfa597f8db9a7fef6e8b9606d4ad99
Thercístenes <thercistenes1@outlook.com>
2023-06-10 10:53:02 -0300

    fundamentos/secao-03-introducao-a-javascript/dia4-javascript-objetos/exercicio-objeto/script1.js

---
+++ fundamentos/secao-03-introducao-a-javascript/dia4-javascript-objetos/exercicio-objeto/script1.js
@@ -0,0 +1,61 @@
+const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      },
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      },
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    },
+  },
+  payment: {
+    total: 60,
+  },
+};
+
+const customerInfo = (fullOrder) => {
+  const donaRestaurante = fullOrder.order.delivery.deliveryPerson;
+  const cliente = fullOrder.name;
+  const tel = fullOrder.phoneNumber;
+  const endereco = fullOrder.address.street;
+  const numero = fullOrder.address.number;
+  const apartamento = fullOrder.address.apartment;
+
+  return `Olá, ${donaRestaurante}, entrega para: ${cliente}, Telefone: ${tel}, ${endereco}, Número: ${numero}, AP: ${apartamento}`;
+}
+
+customerInfo(order);
+
+// Complete a função orderModifier() para que seu retorno seja: 'Olá, Luiz Silva, o valor total de seu pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00.'.
+// Modifique o nome da pessoa compradora para Luiz Silva;
+// Modifique o valor total da compra para R$ 50,00.
+const orderModifier = (fullOrder) => {
+  fullOrder.name = 'Luiz Silva'; 
+  fullOrder.payment.total = 50; 
+
+  const pizzas= Object.keys(fullOrder.order.pizza).join(', ');
+  return `Olá ${fullOrder.name }, o valor  total do seu pedido de ${pizzas} e ${fullOrder.order.drinks.coke.type} é R$ ${fullOrder.payment.total},00.`;
+}
+
+console.log(orderModifier(order));
