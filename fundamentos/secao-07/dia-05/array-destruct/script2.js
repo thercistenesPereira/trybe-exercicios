61ea7f239fa310f20b5752197b4c9db98ee405c8
Thercístenes <thercistenes1@outlook.com>
2023-06-13 13:55:38 -0300

    fundamentos/secao-07/dia-05/array-destruct/script2.js

---
+++ fundamentos/secao-07/dia-05/array-destruct/script2.js
@@ -0,0 +1,6 @@
+const primeNumbers = [17, 23, 37]
+
+const sum = (a, b) => console.log(a + b);
+
+const  [primeNumber, secondNumber, thirdNumber] = primeNumbers;
+sum(primeNumber, secondNumber, thirdNumber)
