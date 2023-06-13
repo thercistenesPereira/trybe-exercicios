79958b003effce702ab5872ac7c8ce3342419ed9
Thercístenes <thercistenes1@outlook.com>
2023-06-13 13:32:41 -0300

    modified:   fundamentos/secao-07/dia-05/array-destruct/script1.js

---
+++ fundamentos/secao-07/dia-05/array-destruct/script1.js
@@ -0,0 +1,7 @@
+const arrayCountries = ['Brazil', 'Japan', 'China', 'Canada'];
+const [firstCountry, secondCountry, thirdCountry, fourthCountry] = arrayCountries;
+
+console.log(firstCountry);
+console.log(secondCountry);
+console.log(thirdCountry);
+console.log(fourthCountry);
