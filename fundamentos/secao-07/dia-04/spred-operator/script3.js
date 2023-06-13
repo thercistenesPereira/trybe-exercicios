8e4c6199c32e2e038b492a56c07b9c162012bdaf
Thercístenes <thercistenes1@outlook.com>
2023-06-13 10:00:49 -0300

    fundamentos/secao-07/dia-04/spred-operator/script3.js

---
+++ fundamentos/secao-07/dia-04/spred-operator/script3.js
@@ -0,0 +1,5 @@
+// console.log(Math.max(57, 8, 5, 800, 152, 74, 630, 98, 40)); // 800
+// console.log(Math.max(57, 8, 5, 800, 152, 74, 630, 98, 40));
+
+const randomNumbers = [57, 8, 5, 800, 152, 74, 630, 98, 40];
+console.log(Math.max(...randomNumbers));
