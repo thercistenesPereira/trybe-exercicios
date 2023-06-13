936ead74cd6b21a62fe8d67192ce76250a5f370e
Thercístenes <thercistenes1@outlook.com>
2023-06-13 15:00:42 -0300

    fundamentos/secao-07/dia-05/Default Parameters/

---
+++ fundamentos/secao-07/dia-05/Default Parameters/script0.js
@@ -0,0 +1,7 @@
+// const greeting = (user = 'pessoa usuária') => console.log(`Welcome ${user}!`);
+
+// greeting(); // // Welcome pessoa usuária!
+
+const greeting = (user = 'pessoa usuária') => console.log(`Welcome ${user}`);
+
+greeting();
