4a7472678c88c41047255a10ab59ee07cc0a771d
Thercístenes <thercistenes1@outlook.com>
2023-06-13 15:41:03 -0300

    fundamentos/secao-07/dia-05/Default Parameters/script1.js

---
+++ fundamentos/secao-07/dia-05/Default Parameters/script1.js
@@ -0,0 +1,5 @@
+const multiply = (number , value = 1) => number * value;
+
+console.log(multiply(8));
+
+// ara praticar, escreva uma função multiply que multiplique dois números passados como argumentos. Atribua como default o valor 1, caso não seja passado nenhum valor como segundo parâmetro.
