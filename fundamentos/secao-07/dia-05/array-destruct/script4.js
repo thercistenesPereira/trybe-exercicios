759912fcb665c8f49adb9dfbbecbc598b091e9a6
Thercístenes <thercistenes1@outlook.com>
2023-06-13 14:25:55 -0300

    fundamentos/secao-07/dia-05/array-destruct/script5.js

---
+++ fundamentos/secao-07/dia-05/array-destruct/script4.js
@@ -0,0 +1,8 @@
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+const [num1, num3, num5, num6, num8, num10, num12] = numerosPares;
+console.log(num1, num3, num5, num6, num8, num10, num12);
+
+// console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log acima
