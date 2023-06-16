006dc1689b02904e3b3c78555c9ef2cd6e24c18f
Thercístenes <thercistenes1@outlook.com>
2023-06-16 15:39:26 -0300

    jest

--- fundamentos/secao-06/dia-02/escrevendo-teste/script.js
+++
@@ -1,9 +0,0 @@
-const numeros = (array) => {
-    for (let index = 0; index < array.length; array += 1) {
-        if (typeof array !== 'number') {
-            return false;
-        }
-    }
-};
-
-module.exports = numeros;
--- fundamentos/secao-06/dia-02/escrevendo-teste/script.test.js
+++
@@ -1,8 +0,0 @@
-// const { describe } = require('yargs');
-const numeros = require('./script');
-
-describe('Requisito 1', () => {
- it('A função recebe  [1, 2, 3, 4, 5] e retorna  true', () => {
-    expect(true).toEqual(numeros([1, 2, 3, 4, 5]));
- });
-});
