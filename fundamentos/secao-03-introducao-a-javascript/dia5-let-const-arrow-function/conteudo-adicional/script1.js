fe80b9bef04df82b7e8935fe64deb0e192a89f97
Thercístenes <thercistenes1@outlook.com>
2023-06-19 09:15:49 -0300

    fundamentos/secao-04-dom-eventos-web-storage/dia-03-eventos/event-listener/

---
+++ fundamentos/secao-03-introducao-a-javascript/dia5-let-const-arrow-function/conteudo-adicional/script1.js
@@ -0,0 +1,10 @@
+var elements = [
+    'Hydrogen',
+    'Helium',
+    'Lithium',
+    'Beryllium'
+  ];
+
+// const array  = elements.map((element) => element.length)
+const array = elements.map(element => element.length);
+console.log(array);
