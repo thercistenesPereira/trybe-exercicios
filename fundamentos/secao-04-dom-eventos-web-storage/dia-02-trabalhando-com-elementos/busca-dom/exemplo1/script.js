25c1f5bcb74b33334e565896bf2bc6e394763095
Thercístenes <thercistenes1@outlook.com>
2023-06-20 09:18:54 -0300

    fundamentos/secao-04-dom-eventos-web-storage/dia-02-trabalhando-com-elementos/busca-dom/

---
+++ fundamentos/secao-04-dom-eventos-web-storage/dia-02-trabalhando-com-elementos/busca-dom/exemplo1/script.js
@@ -0,0 +1,4 @@
+const firstChlild = document.querySelector('#first-child');
+const parentFirstChild = firstChlild.nextElementSibling.nextElementSibling.previousElementSibling;
+
+console.log(parentFirstChild);
