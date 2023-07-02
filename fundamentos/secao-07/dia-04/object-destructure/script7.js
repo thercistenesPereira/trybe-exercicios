b9fba7d1f9e4ae07082a0bd3ffa3c7f4933970ce
Thercístenes <thercistenes1@outlook.com>
2023-07-01 21:24:39 -0300

    fundamentos/secao-07/dia-04/object-destructure/script7.js

---
+++ fundamentos/secao-07/dia-04/object-destructure/script7.js
@@ -0,0 +1,7 @@
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+[i1, i2, i3, i4, i5, i6, i7] = numerosPares
+numerosPares = [ i4, i5, i6, i7]
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log acima
