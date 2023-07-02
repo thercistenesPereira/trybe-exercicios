a16312d0e91fe28577d6a4c6c1e00ce151f20eac
Thercístenes <thercistenes1@outlook.com>
2023-07-02 11:12:53 -0300

    fundamentos/secao-07/dia-04/parametro-rest/script6.js

---
+++ fundamentos/secao-07/dia-04/parametro-rest/script6.js
@@ -0,0 +1,8 @@
+const daysOfWeek = {
+  workDays: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+  weekend: ['Saturday', 'Sunday'],
+};
+
+const { workDays, weekend } = daysOfWeek;
+const weekDays = [...workDays, ...weekend];
+console.log(weekDays);
