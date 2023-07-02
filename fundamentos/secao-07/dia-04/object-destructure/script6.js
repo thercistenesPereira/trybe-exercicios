843666c2c8064f16cd1f4cbfc6368d86ad62e280
Thercístenes <thercistenes1@outlook.com>
2023-07-01 21:13:00 -0300

    fundamentos/secao-07/dia-04/object-destructure/script6.js

---
+++ fundamentos/secao-07/dia-04/object-destructure/script6.js
@@ -0,0 +1,9 @@
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+[comida, bebida, animal] = [animal, bebida, comida]
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
