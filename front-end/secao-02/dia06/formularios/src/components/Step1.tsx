a71905eb81e75d0d1b703260c6de2810f239b33f
Thercístenes <thercistenes1@outlook.com>
2023-07-10 09:43:16 -0300

    ../../../../fundamentos/secao-07/dia-03/aula-ao-vivo/script3.js

---
+++ front-end/secao-02/dia06/formularios/src/components/Step1.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Step1() {
+  return (
+    <div>
+      <div>
+        <h2>Bem Vindo</h2>
+        <p>Seja bem vindo ao formulários de várias etapas</p>
+        <button>Próxima Etapa</button>
+      </div>
+    </div>
+  )
+}
+
+export default Step1
